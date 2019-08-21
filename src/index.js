0e127e8292ec3e563df9e8ce824e3417a40b16f8
Ishan Ghorela <ishan.ghorela@infoedge.com>
2019-08-21 18:41:19 +0530

    pubsub file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-alert("working");
+import pubsub from './pubsub/PubSub';
+
+// let obj = new Pubsub();
+let eventHandler = (data) => {
+    alert('shout ' + data);
+}
+
+pubsub.subscribe('shout',eventHandler);
+
+setTimeout(function(){
+    pubsub.publish('shout', 'yayy')
+}, 5000);
--- src/pubsub/PubSub.js
+++ src/pubsub/PubSub.js
@@ -1,11 +1,27 @@
-export default class Pubsub {
+class Pubsub {
     subscriptions = {};
 
-    subscribe = () => {
+    subscribe = (event, eventHandler, context) => {
+            eventHandler.bind(context);
+            if(!this.subscriptions[event]) {
+                this.subscriptions[event] = [];
+            }
+            let position = this.subscriptions[event].push(eventHandler);
+            let index = position - 1;
+            return({
+                unsubscribe: () => {
+                    this.subscriptions[event].splice(index, 1);
+                }   
+            });
+        };
 
-    };
-
-    publish = () => {
-
+    publish = (event, payload) => {
+        if(!this.subscriptions[event]) return;
+        this.subscriptions[event].forEach(eventHandler => {
+            eventHandler(payload);
+        });
     };
 }
+
+let instance = new Pubsub();
+export default instance;
