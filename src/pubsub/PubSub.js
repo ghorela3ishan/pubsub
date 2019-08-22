7361d7f1c5ec04f2bd3031ec302586fdbd86e43f
Ishan Ghorela <ishan.ghorela@infoedge.com>
2019-08-22 15:40:57 +0530

    added type for parameters

--- src/pubsub/PubSub.js
+++ src/pubsub/PubSub.js
@@ -1,9 +1,16 @@
 class Pubsub {
-    // object that contains key value pairs of event and an array of associated callbacks
+    /**
+     * object that contains key value pairs of event (event type will be string) and an array of associated callbacks
+     */ 
     subscriptions = {};
 
-    // adds the received eventHandler to the array of received event after binding with provided context
-    // and returns an object that contains callback to remove the eventHandler from the event array 
+    /**
+     * adds the received eventHandler to the array of received event after binding with provided context
+     * and returns an object that contains callback to remove the eventHandler from the event array
+     * @param { string }  event
+     * @param { eventHandler } callback
+     * @param { context } object
+     */
     subscribe = (event, eventHandler, context = window) => {
             let eventHandlerWithContext = eventHandler.bind(context);
             if(!this.subscriptions[event]) {
@@ -19,7 +26,12 @@ class Pubsub {
             });
         };
 
-    // invokes all the callbacks contained in the event (received in parameter) array with the payload received 
+    /**
+     * invokes all the callbacks contained in the event (received in parameter) property of 
+     * subscriptions array with the payload received 
+     * @param { event } string
+     * @param { payload } object
+     */
     publish = (event, payload) => {
         if(!this.subscriptions[event]) return;
         this.subscriptions[event].forEach(eventHandler => {
